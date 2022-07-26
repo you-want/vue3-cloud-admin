875d0991f17ef4822b76ee9350d0b9956a418edd
BiggerRain <15911122312@163.COM>
2022-07-26 18:35:09 +0800

    feat: layout

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+function bootstrap() {
+  const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+  app.use(createPinia());
+  app.use(router);
 
-app.mount('#app')
+  app.mount("#app");
+}
+
+bootstrap();
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,33 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import Components from 'unplugin-vue-components/vite'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), 
+    vue(),
     vueJsx(),
     Components({
-      resolvers: [
-        AntDesignVueResolver()
-      ],
-    })
+      resolvers: [AntDesignVueResolver()],
+    }),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: "src/auto-import.d.ts",
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    open: true,
+  },
+});
